12ace6904f6a7e58af571ac678993b2ef0fcbd78
Bernardo Bertante <bernardo.sctp@gmail.com>
2025-04-23 13:09:54 +0100

    add: creation of mission loader

--- src/com/midnightpinewoodclub/Application.java
+++ src/com/midnightpinewoodclub/Application.java
@@ -2,10 +2,14 @@ package com.midnightpinewoodclub;
 
 import com.midnightpinewoodclub.controller.MainController;
 import com.midnightpinewoodclub.factory.AppFactory;
+import com.midnightpinewoodclub.repository.IMissionRepository;
+import com.midnightpinewoodclub.util.MissionLoader;
 
 public class Application {
     public static void main(String[] args) {
         MainController mainController = AppFactory.createMainController();
+        IMissionRepository missionRepository = AppFactory.createMissionRepository();
+        missionRepository.setMissions(MissionLoader.loadMissions("/resources/mission.json"));
         mainController.startMenu();
         AppFactory.closeScanner();
     }
